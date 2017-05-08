4b2360bdf00ed128f7548b6a23059fa4273882c9
Jeff Stafford <jeff.stafford@live.com>
2017-05-07 20:30:33 -0400

    root redirects to app, remove datatables, make nicer docker image

--- dashing/urls.py
+++ dashing/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^slurm/', include('slurm.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
 
