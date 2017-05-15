fd2992ba805a36d57a1728b5a1456dcd0e3bbdb7
Jeff Stafford <jeff.stafford@live.com>
2017-05-14 22:38:08 -0400

    move to non-django copy of bootstrap, replace theme

--- dashing/settings.py
+++ dashing/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'slurm.apps.SlurmConfig',
-    'bootstrap3',
-    #'bootstrap_themes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
