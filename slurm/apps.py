f8cf1948bb047f0d7b87eb0c2bff187fb8c3ad9d
Jeff Stafford <jeff.stafford@live.com>
2017-09-13 16:24:58 -0400

    always unlock redis locks on start in case of unclean exit

--- slurm/apps.py
+++ slurm/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
-
 class SlurmConfig(AppConfig):
     name = 'slurm'
+
+    def ready(self):
+        # unlock the database in case of a non-smooth exit from last time
+        from .tasks import RedisLock
+        RedisLock('db').release()
