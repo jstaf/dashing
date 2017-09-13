8e17214b99bb8f60d55ea3644c2f5614f5795117
Jeff Stafford <jeff.stafford@live.com>
2017-09-13 14:38:44 -0400

    pager does not appear for single pages of results

--- slurm/ui/pager.py
+++ slurm/ui/pager.py
@@ -6,6 +6,10 @@ def pager(queryset, redirect_path, page=0, page_size=50):
     Create a paging object to allow paging through results.
     """
     n_pages = math.ceil(len(queryset) / page_size)
+    if n_pages == 1:
+        # no pager needed
+        return ''
+
     prev_page = np.clip(page - 1, 0, n_pages)
     next_page = np.clip(page + 1, 0, n_pages)
 
